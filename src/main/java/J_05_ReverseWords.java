6014a46e91100f06d1fa9c8cd0d33787c273e4f2
Aneesh <aneesh@sky.com>
2026-01-11 15:57:01 +1100

    reverse words

---
+++ src/main/java/J_05_ReverseWords.java
@@ -0,0 +1,38 @@
+public class J_05_ReverseWords {
+
+    public static void main(String[] args) {
+
+        System.out.println(reverseWords("   hello world   "));
+        System.out.println(reverseWords("a dog is in my house"));
+        System.out.println(reverseWords("a cat     jumps over 20 fences"));
+
+    }
+
+    private static String reverseWords(String sentence){
+
+        //create sb
+        StringBuilder sb = new StringBuilder();
+        StringBuilder currentWord = new StringBuilder();
+
+        for (int i = 0; i < sentence.length(); i++){
+            char currentChar = sentence.charAt(i);
+            if(currentChar == ' '){
+                //define word if currentWord is populated
+                if(!currentWord.isEmpty()){
+                    sb.insert(0, currentWord);
+                    sb.insert(0, ' ');
+                    currentWord = new StringBuilder();
+                }
+            }
+            else{
+                currentWord.append(currentChar);
+                if(i == sentence.length() -1){
+                    sb.insert(0, currentWord);
+                    sb.insert(0, ' ');
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+}
