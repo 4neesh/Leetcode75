73fc0b92dac5b6c1cbebd5c310a60d61b38f05de
Aneesh <aneesh@sky.com>
2026-01-18 08:51:18 +1100

    stringcompress

---
+++ src/main/java/J_09_StringCompression.java
@@ -0,0 +1,35 @@
+public class J_09_StringCompression {
+
+
+    public static void main(String[] args) {
+
+        int a = compressString(new char[]{'a','a','b','b'});
+        int b = compressString(new char[]{'a','a','a','b'});
+        int c = compressString(new char[]{'a','a','a','a'});
+
+    }
+
+    private static int compressString(char[] s){
+        int index = 0;
+        int start = 0;
+
+        //loop through the letters while the next character same as start
+        //when we get to the end or the character is not the same, update the index and add the end - start
+        while(start < s.length){
+            int end = start;
+            while(end < s.length && s[end] == s[start]){
+                end++;
+            }
+            int count = end - start;
+            s[index++] = s[start];
+            if(count >= 2){
+                char[] countArr = Integer.toString(end - start).toCharArray();
+                for(char c: countArr){
+                    s[index++] = c;
+                }
+            }
+            start += count;
+        }
+        return index;
+    }
+}
