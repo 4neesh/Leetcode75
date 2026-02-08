24ebd1b4b55e0ebf56cf6e4b4e207d3f460bb8d8
Aneesh <aneesh@sky.com>
2026-02-09 08:11:38 +1100

    delete middle node

---
+++ src/main/java/J_30_DeleteMiddleNode.java
@@ -0,0 +1,57 @@
+
+public class J_30_DeleteMiddleNode {
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(3);
+        head.next = new ListNode(4);
+        head.next = new ListNode(7);
+        head.next = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next = new ListNode(6);
+
+        head = deleteMiddle(head);
+        System.out.println(head);
+
+    }
+
+    public static ListNode deleteMiddle(ListNode head){
+
+        ListNode current = head;
+        ListNode midPrev = null;
+        ListNode middle = head;
+
+        while (current.next != null){
+            if(current.next.next != null) {
+                current = current.next.next;
+                midPrev = middle;
+                middle = middle.next;
+            }
+            else{
+                current = current.next;
+                midPrev = middle;
+                middle = middle.next;
+            }
+        }
+
+        if(midPrev != null) {
+            midPrev.next = middle.next;
+        }
+        else{
+            return null;
+        }
+
+        return head;
+    }
+
+}
+
+
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
