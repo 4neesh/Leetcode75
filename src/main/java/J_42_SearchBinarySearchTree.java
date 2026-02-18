221ed54655cbaf8184f423ba51ee484fa554d008
Aneesh <aneesh@sky.com>
2026-02-18 14:19:20 +1100

    clean up treenode and seaerch binary tree

--- src/main/java/J_42_SearchBinarySearchTree.java
+++ src/main/java/J_42_SearchBinarySearchTree.java
@@ -1,2 +1,29 @@
+import util.TreeNode;
+
 public class J_42_SearchBinarySearchTree {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+
+        while(root != null){
+
+            if(root.val == val){
+                return root;
+            }
+            if(val < root.val){
+                root = root.left;
+            }
+            else{
+                root = root.right;
+            }
+        }
+        return null;
+    }
+
+
 }
